020881575b487cc7632a18f64b98a3f0bddf1616
Toyofumi Fujiwara <sh1@dream.jp>
2014-11-13 18:21:07 +0900

    addon test ok

--- comicLineartNode.py
+++ comicLineartNode.py
@@ -1,10 +1,9 @@
 # this script convert setting to monochrome lineart for comics
 import bpy
 
-    def comicLineartNode():
+def comicLineartNode():
     # this script convert setting to monochrome lineart for comics
-    import bpy
-
+     
     s = bpy.context.scene
 
     edge_threshold = 44
@@ -78,6 +77,14 @@ import bpy
     setAlpha.location = (700, 500)
     dilate.location = (400, 400)
 
+    rgb = n.new("CompositorNodeRGB")
+    alphaMerge = n.new("CompositorNodeAlphaOver")
+    setAlphaMerge = n.new("CompositorNodeSetAlpha")
+
+    rgb.location = (400, -200)
+    alphaMerge.location = (900, -200)
+    setAlphaMerge.location= (700, -200)
+
     l.new(render.outputs[0], rgb2Bw.inputs[0])
     l.new(rgb2Bw.outputs[0], val2Rgb.inputs[0])
     l.new(val2Rgb.outputs[0], setAlpha.inputs[0])
@@ -88,6 +95,10 @@ import bpy
     l.new(freestyleRender.outputs[0], alpha.inputs[2])
     l.new(alpha.outputs[0], composite.inputs[0])
 
+    l.new(rgb.outputs[0],setAlphaMerge.inputs[0])
+    l.new(dilate.outputs[0], setAlphaMerge.inputs[1])
+    l.new(setAlphaMerge.outputs[0], alphaMerge.inputs[1])
+    l.new(freestyleRender.outputs[0], alphaMerge.inputs[2])
 
     # gray setting
     val2Rgb.color_ramp.interpolation = 'CONSTANT'
@@ -101,24 +112,32 @@ import bpy
     val2Rgb.color_ramp.elements[1].position=0.08
     val2Rgb.color_ramp.elements[1].position=0.0
 
+    rgb.outputs[0].default_value = (1,1,1,1)
 
     # output to image file
     import os
     lineout = n.new("CompositorNodeOutputFile")
     lineout.name = "line out"
     lineout.location = (200, 600)
-    lineout.base_path = os.path.expanduser("~/Desktop/l")
+    lineout.base_path = os.path.expanduser("~/Desktop/rendering/l")
     lineout.file_slots.new("rendering")
 
     grayout = n.new("CompositorNodeOutputFile")
     grayout.name = "gray out"
     grayout.location = (1200, 600)
-    grayout.base_path = os.path.expanduser("~/Desktop/g")
+    grayout.base_path = os.path.expanduser("~/Desktop/rendering/g")
     grayout.file_slots.new("rendering")
 
+    mergeout = n.new("CompositorNodeOutputFile")
+    mergeout.name = "merge out"
+    mergeout.location = (1200, -100)
+    mergeout.base_path = os.path.expanduser("~/Desktop/rendering/m")
+    mergeout.file_slots.new("rendering")
 
     l.new(freestyleRender.outputs[0], lineout.inputs[-1])
     l.new(setAlpha.outputs[0], grayout.inputs[-1])
+    l.new(alphaMerge.outputs[0], mergeout.inputs[-1])
+
 
 
 
@@ -136,12 +155,12 @@ import bpy
 
 class ComicLineartNode(bpy.types.Operator):
     """lineart converter by Node"""
-    bl_idname = "lineartNode.comic"
+    bl_idname = "lineartgray.comic"
     bl_label = "comic lineart node"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):    
-        lineartNode()
+        comicLineartNode()
         return {'FINISHED'}
 
 
